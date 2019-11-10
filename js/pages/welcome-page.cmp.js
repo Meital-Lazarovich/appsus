4b3f13cef24077c12cf55bfc14c6120d35ca7c5f
Meital Lazarovich <lazarovich.meital@gmail.com>
2019-11-10 18:27:29 +0200

    better welcome css

--- js/pages/welcome-page.cmp.js
+++ js/pages/welcome-page.cmp.js
@@ -4,7 +4,7 @@
 export default {
     name: 'welcome-page',
     template: `
-        <section class="welcome-page text-center">
+        <section class="welcome-page text-center flex align-center justify-center column">
             <img src="https://www.employmenthelp.org/wp-content/uploads/Workshops-Header-Background.png"/>
             <h1>Start Organizing!</h1>
             <div class="links flex align-center space-between">
@@ -12,8 +12,8 @@ export default {
                 <router-link to="/keep">Keep</router-link>
                 <router-link to="/book">Books</router-link>
             </div>
-            <footer class="footer flex column justify-center">
-                <div><i class="fa fa-copyright"></i> Meital & Itay All Rights Reserved 2019</div>
+            <footer class="footer flex align-center justify-center">
+                <i class="fa fa-copyright"></i> Meital & Itay All Rights Reserved 2019
             </footer>
         </section>
     `
