39f6fda4c76a3074d6487cd47fffe2b95667ce32
Meital Lazarovich <lazarovich.meital@gmail.com>
2019-11-10 17:53:30 +0200

    welcome page links

--- js/pages/welcome-page.cmp.js
+++ js/pages/welcome-page.cmp.js
@@ -7,6 +7,11 @@ export default {
         <section class="welcome-page text-center">
             <img src="https://www.employmenthelp.org/wp-content/uploads/Workshops-Header-Background.png"/>
             <h1>Start Organizing!</h1>
+            <div class="links flex align-center space-between">
+                <router-link to="/email/inbox">Email</router-link>
+                <router-link to="/keep">Keep</router-link>
+                <router-link to="/book">Books</router-link>
+            </div>
         </section>
     `
 }
